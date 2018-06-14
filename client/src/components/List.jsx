6df293c2aa7d39f2d43b40f997174dc9c25f6f6a
Matthew Strom <strom@Matthews-MacBook-Air.local>
2018-06-14 15:47:25 -0700

    made compatible with proxy server

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -6,8 +6,8 @@ class List extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      liked: false,
       lists2listings: [],
+      liked: false,
     };
     this.likeList = this.likeList.bind(this);
   }
@@ -17,22 +17,33 @@ class List extends React.Component {
   }
 
   getLists2Listings() {
+    // if (this.state.lists2listings.listings.length > 0) {
+      // this.setState({
+      //   liked: !this.state.liked,
+      // });
+    // }
+
     const listingIds = this.props.listings.map(listing => listing.id);
     axios.get('/lists2listings', { params: { listingIds } })
       .then((res) => {
+        console.log('getl2l promise ran');
         this.setState({
           lists2listings: res.data,
         }, this.checkLiked);
-      });
+      })
+      .catch(() => console.log('err'));
   }
 
   likeList() {
+    console.log('likeList ran');
     axios.post('/lists', { listId: this.props.list.id, listingId: this.props.listingId, liked: this.state.liked })
       .then(() => {
-        this.setState({
-          liked: !this.state.liked,
-        }, this.getLists2Listings);
+        console.log('likeList promise ran')
+        this.getLists2Listings();
       });
+    this.setState({
+      liked: !this.state.liked,
+    });
   }
 
   checkLiked() {
@@ -47,8 +58,12 @@ class List extends React.Component {
       <div>
         <div className={styles.BB} />
         <div className={styles.listItem}> {this.props.list.name} </div>
-        {!this.state.liked && <button className={styles.miniHeartsEmpty} onClick={this.likeList} />}
-        {this.state.liked && <button className={styles.miniHeartsFull} onClick={this.likeList} />}
+        {/* {!this.state.liked && <button className={style.miniHeartsEmpty} onClick={this.likeList} />}
+        {this.state.liked && <button className={style.miniHeartsFull} onClick={this.likeList} />} */}
+        <button onClick={this.likeList} >
+          {!this.state.liked && <img src="https://s3-us-west-1.amazonaws.com/bnbresources/heartOutline.png" alt="" className={styles.miniHeartsEmpty} />}
+          {this.state.liked && <img src="https://s3-us-west-1.amazonaws.com/bnbresources/heartFull.png" alt="" className={styles.miniHeartsFull} />}
+        </button>
       </div>
     );
   }
